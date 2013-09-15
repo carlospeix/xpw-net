8527bf363987f0c8304ebd03ef6fe03afbe4520c
Carlos Peix <carlos.peix@gmail.com>
2013-09-15 11:14:33 -0300

    Persistencia funcionando

---
+++ src/Web/App_Start/AppConfig.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Web.Routing;
+using System.Web.Optimization;
+using Castle.Windsor;
+using Castle.MicroKernel.Registration;
+using NHibernate;
+
+namespace xpw.Web
+{
+    public class AppConfig
+    {
+        private static IWindsorContainer _container;
+
+        public static void Initialize()
+        {
+            CreateContainer();
+            RegisterControllerFactory();
+            InitializeNHibernate();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters, _container);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //RegisterRepositories();
+            //RegisterQueries();
+
+            //MetadataBuilderConfig.Start(_container);
+        }
+
+        public static void Dispose()
+        {
+            _container.Dispose();
+        }
+
+        private static void CreateContainer()
+        {
+            _container = new WindsorContainer();
+
+            // register the container with itself to be able to resolve 
+            // the dependency in the ctor of WindsorControllerFactory
+            _container.Register(Component.For<IWindsorContainer>().
+                Instance(_container).LifeStyle.Singleton);
+        }
+
+        private static void RegisterControllerFactory()
+        {
+            _container.Register(
+                Component.For<IControllerFactory>()
+                    .ImplementedBy<WindsorControllerFactory>()
+                    .LifeStyle.Singleton);
+
+            ControllerBuilder.Current.SetControllerFactory(_container.Resolve<IControllerFactory>());
+        }
+
+        private static void InitializeNHibernate()
+        {
+            var inititializer = new NHibernateConfig();
+            inititializer.InitializeForWeb();
+            inititializer.UpdateSchema();
+            //inititializer.CreateSchema();
+
+            _container.Register(
+                Component.For<ISessionFactory>()
+                    .Instance(inititializer.SessionFactory)
+                    .LifeStyle.Singleton);
+
+            _container.Register(
+                Component.For<SessionPerActionFilter>()
+                    .ImplementedBy<SessionPerActionFilter>()
+                    .LifeStyle.Transient);
+        }
+
+        //private static void RegisterRepositories()
+        //{
+        //    _container.Register(
+        //        Component.For(typeof(IRepository<>))
+        //            .ImplementedBy(typeof(Repository<>))
+        //            .LifeStyle.Transient);
+        //}
+
+        //private static void RegisterQueries()
+        //{
+        //    _container.Register(AllTypes
+        //        .FromAssembly(Assembly.GetAssembly(typeof(Query<>)))
+        //        .BasedOn(typeof(IQuery<>))
+        //        .WithService.AllInterfaces());
+        //}
+
+        //private static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        //{
+        //    //filters.Add(new RequestTimingFilter(), 1);
+        //    filters.Add(new HandleErrorAttribute(), 2);
+        //    filters.Add(_container.Resolve<SessionPerActionFilter>(), 3);
+        //}
+    }
+}
---
+++ src/Web/App_Start/FilterConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Castle.Windsor;
+
+namespace xpw.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters, IWindsorContainer container)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(container.Resolve<SessionPerActionFilter>(), 3);
+        }
+    }
+}
